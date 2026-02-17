db3c52fbc8e5009e13811cca24b95b5d2c984b07
lordiring90 <lordiring2000@gmail.com>
2026-02-17 00:02:53 -0500

    ya arregle la llave extra, ahora no hay problemas

--- src/Principal.java
+++ src/Principal.java
@@ -1,37 +1,18 @@
-import java.util.Scanner; // 1. Importamos la herramienta para leer el teclado
+import java.util.Scanner; // Siempre arriba de todo
 
 public class Principal {
     public static void main(String[] args) {
-        // 2. Creamos el "lector"
+        //Todo tu código debe vivir aquí adentro
         Scanner lector = new Scanner(System.in);
 
-        // 3. Pedimos un dato (String = Texto)
         System.out.println("¿Cómo te llamas?");
         String nombre = lector.nextLine();
 
-        // 4. Pedimos un número (int = Entero)
         System.out.println("¿Cuántos años tienes?");
         int edad = lector.nextInt();
 
-        // 5. Mostramos el resultado combinando texto y variables
         System.out.println("Hola " + nombre + ", el próximo año tendrás " + (edad + 1) + " años.");
 
-        lector.close(); // Buena práctica: cerrar el lector al terminar
-
-        //Nuevo aqui yo miro
-        String Name = "Legolas";
-        int Edad2 = 30;
-        double dinero = 0.05;
-
-        //usaremos la salida del sistema para ver si esto si sale
-        System.out.println("un personaje nuevo a continuacion ->");
-        System.out.println("Se llama " + Name);
-        System.out.println("Tiene "+ Edad2 + " años");
-        System.out.println("En su cuenta bancaria tiene: " + dinero + " De dollars");
-
-
-
-
-
+        lector.close();
     }
-}
+} // Esta es la llave que cierra la Clase. No debe haber nada después de esta.
